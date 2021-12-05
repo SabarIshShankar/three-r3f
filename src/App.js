bd1e53ef4b8d4290b1adbc2f9bc4faa5fef56cc2
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-05 11:44:19 +0530

    sphere

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+import * as THREE from "THREE";
+import { render } from "react-dom";
+import React, { useRef, useState, useMemo, useEffect } from "react";
+import { Canvas, extend, useThree, useFrame } from "@react-three/fiber";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
 
-export default function App() {
+extend({ EffectComposer, RenderPass, UnrealBloomPass });
+
+function Sphere({ geometry, x, y, x, s }) {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.position.x =
+      x + Math.sin((state.clock.getElapsedTime() * s) / 2);
+    ref.current.position.y =
+      z + Math.sin((state.clock.getElapsedTime() * s) / 2);
+    ref.current.position.z =
+      z + Math.sin((state.clock.getElapsedTime() * s) / 2);
+  });
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Mesh ref={ref} position={[x, y, z]} scale={[s, s, s]} geometry={geometry}>
+      <meshStandardMaterial color="hotpink" roughness={1} />
+    </Mesh>
   );
 }
